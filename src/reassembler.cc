ecd4a8beccf552d8a2a79e93ae527f8de06f4af2
Tyooughtul <yooughtul@126.com>
2026-01-18 14:56:47 +0800

    Test project /root/minnow/build       Start  1: compile with bug-checkers  1/18 Test  #1: compile with bug-checkers ........   Passed   17.80 sec       Start  3: byte_stream_basics  2/18 Test  #3: byte_stream_basics ...............   Passed    0.01 sec       Start  4: byte_stream_capacity  3/18 Test  #4: byte_stream_capacity .............   Passed    0.01 sec       Start  5: byte_stream_one_write  4/18 Test  #5: byte_stream_one_write ............   Passed    0.01 sec       Start  6: byte_stream_two_writes  5/18 Test  #6: byte_stream_two_writes ...........   Passed    0.06 sec       Start  7: byte_stream_many_writes  6/18 Test  #7: byte_stream_many_writes ..........   Passed    0.12 sec       Start  8: byte_stream_stress_test  7/18 Test  #8: byte_stream_stress_test ..........   Passed    0.08 sec       Start  9: reassembler_single  8/18 Test  #9: reassembler_single ...............   Passed    0.01 sec       Start 10: reassembler_cap  9/18 Test #10: reassembler_cap ..................   Passed    0.01 sec       Start 11: reassembler_seq 10/18 Test #11: reassembler_seq ..................   Passed    0.08 sec       Start 12: reassembler_dup 11/18 Test #12: reassembler_dup ..................   Passed    0.04 sec       Start 13: reassembler_holes 12/18 Test #13: reassembler_holes ................   Passed    0.07 sec       Start 14: reassembler_overlapping 13/18 Test #14: reassembler_overlapping ..........   Passed    0.01 sec       Start 15: reassembler_win 14/18 Test #15: reassembler_win ..................   Passed    0.48 sec       Start 37: no_skip 15/18 Test #37: no_skip ..........................   Passed    0.01 sec       Start 38: compile with optimization 16/18 Test #38: compile with optimization ........   Passed    9.92 sec       Start 39: byte_stream_speed_test         ByteStream throughput (pop length 4096): 12.70 Gbit/s         ByteStream throughput (pop length 128):   0.90 Gbit/s         ByteStream throughput (pop length 32):    0.38 Gbit/s 17/18 Test #39: byte_stream_speed_test ...........   Passed    1.70 sec       Start 40: reassembler_speed_test         Reassembler throughput (no overlap):  45.81 Gbit/s         Reassembler throughput (10x overlap):  2.49 Gbit/s 18/18 Test #40: reassembler_speed_test ...........   Passed    0.79 sec
    100% tests passed, 0 tests failed out of 18

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -5,13 +5,110 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  debug( "unimplemented insert({}, {}, {}) called", first_index, data, is_last_substring );
+  if ( is_last_substring ) {
+    last_substring_received_ = true;
+    eof_index_ = first_index + data.size();
+  }
+  if (data.empty()) {
+	  try_close_stream();
+	  return;
+  }
+
+  uint64_t data_end = first_index + data.size();
+  uint64_t stream_capacity = output_.writer().available_capacity();
+  uint64_t stream_size = output_.writer().bytes_pushed();
+
+  if ( first_index >= stream_size + stream_capacity ) {
+    try_close_stream();
+    return;
+  }
+
+  if ( data_end <= next_expected_index_ ) {
+    try_close_stream();
+    return;
+  }
+
+  if ( first_index < next_expected_index_ ) {
+    uint64_t offset = next_expected_index_ - first_index;
+    data = data.substr( offset );// 截取下标到末尾的位置
+    first_index = next_expected_index_;
+  }
+
+  if ( first_index + data.size() > stream_size + stream_capacity ) {
+    data.resize( stream_size + stream_capacity - first_index );
+  }
+
+  if ( data.empty() ) {
+    try_close_stream();
+    return;
+  }
+
+  merge_into_buffer( first_index, data );
+  write_to_stream();
+  try_close_stream();
 }
 
-// How many bytes are stored in the Reassembler itself?
-// This function is for testing only; don't add extra state to support it.
 uint64_t Reassembler::count_bytes_pending() const
 {
-  debug( "unimplemented count_bytes_pending() called" );
-  return {};
+  uint64_t count = 0;
+  for ( const auto& [index, data] : buffer_ ) {
+    count += data.size();
+  }
+  return count;
 }
+
+void Reassembler::merge_into_buffer( uint64_t first_index, string data )
+{
+  if ( data.empty() ) {
+    return;
+  }
+
+  uint64_t data_end = first_index + data.size();
+
+  auto it = buffer_.lower_bound( first_index );
+
+  while ( it != buffer_.end() && it->first <= data_end ) {
+    uint64_t existing_end = it->first + it->second.size();
+    if ( existing_end > data_end ) {
+      data += it->second.substr( data_end - it->first );
+      data_end = existing_end;
+    }
+    it = buffer_.erase( it );
+  }
+
+  if ( it != buffer_.begin() ) {
+    auto prev = std::prev( it );
+    uint64_t prev_end = prev->first + prev->second.size();
+    if ( prev_end >= first_index ) {
+      if ( prev_end < data_end ) {
+        prev->second += data.substr( prev_end - first_index );
+      }
+      return;
+    }
+  }
+
+  buffer_[first_index] = data;
+}
+
+void Reassembler::write_to_stream()
+{
+  while ( !buffer_.empty() && buffer_.begin()->first == next_expected_index_ ) {
+    auto it = buffer_.begin();
+    uint64_t bytes_to_write = min( it->second.size(), output_.writer().available_capacity() );
+    output_.writer().push( it->second.substr( 0, bytes_to_write ) );
+    next_expected_index_ += bytes_to_write;
+    has_written_ = true;
+    if ( bytes_to_write < it->second.size() ) {
+      buffer_[next_expected_index_] = it->second.substr( bytes_to_write );
+    }
+    buffer_.erase( it );
+  }
+}
+
+void Reassembler::try_close_stream()
+{
+  if ( last_substring_received_ && buffer_.empty() && next_expected_index_ == eof_index_ )  {
+    output_.writer().close();
+  }
+}
+
